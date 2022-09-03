2afd57dc143576d9951af82485118ef47fec5e09
Kenny Zhou <l.w.r.f.42@icloud.com>
2022-09-04 00:49:06 +0800

    can train

--- DOAM/data/config.py
+++ DOAM/data/config.py
@@ -65,9 +65,90 @@ OPIXray = {
 DongYing = {
     #'num_classes': 6,  !!!!
     'num_classes': len(OPIXray_CLASSES)+1,
-    'lr_steps': (100, 500, 1000, 1500),
+    'lr_steps': (100, 200, 500, 1000, 2000,5000,10000),
     #'max_iter': 20000,
-    'max_iter': 2000,
+    'max_iter': 1000000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    # 'min_dim': 300/512
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    #'name': 'SIXray',
+    'name': 'DongYing2022-09-01',
+}# config.py
+import os.path
+from .OPIXray import OPIXray_CLASSES
+
+# gets home dir cross platform
+HOME = os.path.expanduser("~")
+
+# for making bounding boxes pretty
+COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
+          (0, 255, 255, 128), (255, 0, 255, 128), (255, 255, 0, 128))
+
+MEANS = (104, 117, 123)
+
+# SSD300 CONFIGS
+voc = {
+    'num_classes': 21,
+    'lr_steps': (80000, 100000, 120000),
+    'max_iter': 120000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'VOC',
+}
+
+coco = {
+    'num_classes': 201,
+    'lr_steps': (280000, 360000, 400000),
+    'max_iter': 400000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'COCO',
+}
+
+
+OPIXray = {
+    #'num_classes': 6,  !!!!
+    'num_classes': 6,
+    'lr_steps': (80000, 100000, 120000),
+    #'max_iter': 20000,
+    'max_iter': 1000000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    # 'min_dim': 300/512
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    #'name': 'SIXray',
+    'name': 'Xray20190723',
+}
+
+DongYing = {
+    #'num_classes': 6,  !!!!
+    'num_classes': len(OPIXray_CLASSES)+1,
+    'lr_steps': (100, 200, 500, 1000, 2000,5000,10000),
+    #'max_iter': 20000,
+    'max_iter': 1000000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     # 'min_dim': 300/512
     'min_dim': 300,
